2fcacffbc59d9223c91089669a321a0b2cfa1bd6
Derrity <derrity0731@gmail.com>
2024-01-07 11:39:13 +0800

    install depend

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,7 @@
 apt install cmake clang gcc g++ libasio-dev curl libcurl4-openssl-dev libssl-dev libboost-all-dev make aria2 git -y
 aria2c -x 10 https://github.com/yhirose/cpp-httplib/archive/refs/tags/v0.14.3.tar.gz
-tar -xvf v0.14.3.tar.gz
-rm -rf v0.14.3.tar.gz
+tar -xvf cpp-httplib-0.14.3.tar.gz
+rm -rf cpp-httplib-0.14.3.tar.gz
 cd cpp-httplib-0.14.3
 mkdir -p build
 cd build
